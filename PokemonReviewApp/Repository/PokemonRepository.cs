32787e515b85a6ee9efefe5a449c14ef90727a86
Mete Sayan <metesayan99@gmail.com>
2023-09-15 00:01:54 +0300

    feat: GetPokemon (By Id) & GetPokemonRating APIs

--- PokemonReviewApp/Repository/PokemonRepository.cs
+++ PokemonReviewApp/Repository/PokemonRepository.cs
@@ -17,5 +17,31 @@ namespace PokemonReviewApp.Repository
         {
             return _context.Pokemon.OrderBy(p => p.Id).ToList();
         }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemon.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            return _context.Pokemon.Where(p => p.Name == name).FirstOrDefault();
+        }
+
+        public decimal GetPokemonRating(int pokeId)
+        {
+            var review = _context.Reviews.Where(r => r.Pokemon.Id == pokeId);
+
+            if (review.Count() <= 0)
+                return 0;
+
+            return ((decimal)review.Sum(r => r.Rating) / review.Count());
+
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            return _context.Pokemon.Any(p => p.Id == pokeId);
+        }
     }
 }
