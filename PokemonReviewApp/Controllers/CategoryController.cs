9650f72abe48ca8a64006104db45e22465726b9b
Mete Sayan <metesayan99@gmail.com>
2023-09-17 22:27:03 +0300

    feat: Category Controller

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<CategoryDto>))]
+        public IActionResult GetCategories()
+        {
+            var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(categories);
+        }
+
+        [HttpGet("{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(CategoryDto))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int categoryId)
+        {
+            if (!_categoryRepository.CategoriesExists(categoryId))
+            {
+                return NotFound();
+            }
+
+            var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(categoryId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(category);
+        }
+
+        [HttpGet("pokemon/{categoryId}")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<PokemonDto>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByCategory(int categoryId)
+        {
+            if (!_categoryRepository.CategoriesExists(categoryId))
+            {
+                return NotFound();
+            }
+
+            var pokemons = _mapper.Map<List<PokemonDto>>(_categoryRepository.GetPokemonByCategory(categoryId));
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(pokemons);
+        }
+    }
+}
