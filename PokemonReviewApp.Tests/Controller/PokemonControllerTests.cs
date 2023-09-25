26def975e59a4230fe2d7d39ed21f93e1cd42b4a
Mete Sayan <metesayan99@gmail.com>
2023-09-25 21:19:03 +0300

    feat: Unit tests for CreatePokemon & GetAll

---
+++ PokemonReviewApp.Tests/Controller/PokemonControllerTests.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using FakeItEasy;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Controllers;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonReviewApp.Tests.Controller
+{
+    public class PokemonControllerTests
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly IReviewRepository _reviewRepository;
+        private readonly IMapper _mapper;
+        public PokemonControllerTests()
+        {
+            _pokemonRepository = A.Fake<IPokemonRepository>();
+            _reviewRepository = A.Fake<IReviewRepository>();
+            _mapper = A.Fake<IMapper>();
+        }
+
+        [Fact]
+        public void PokemonController_GetAll_ReturnOK()
+        {
+            // Arrange
+            var pokemons = A.Fake<ICollection<Pokemon>>();
+            var pokemonList = A.Fake<List<PokemonDto>>();
+            A.CallTo(() => _mapper.Map<List<PokemonDto>>(pokemons)).Returns(pokemonList);
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            // Act
+            var result = controller.GetAll();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.Should().BeOfType(typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public void PokemonController_CreatePokemon_ReturnOK()
+        {
+            // Arrange
+            int ownerId = 1;
+            int catId = 2;
+            var pokemon = A.Fake<Pokemon>();
+            var pokemonCreate = A.Fake<PokemonDto>();
+            var pokemons = A.Fake<ICollection<Pokemon>>();
+            var pokemonList = A.Fake<List<PokemonDto>>();
+            A.CallTo(() => _pokemonRepository.GetPokemonTrimToUpper(pokemonCreate)).Returns(pokemon);
+            A.CallTo(() => _mapper.Map<Pokemon>(pokemonCreate)).Returns(pokemon);
+            A.CallTo(() => _pokemonRepository.CreatePokemon(ownerId, catId, pokemon)).Returns(true);
+            var controller = new PokemonController(_pokemonRepository, _reviewRepository, _mapper);
+
+            // Act
+            var result = controller.CreatePokemon(ownerId, catId, pokemonCreate);
+
+            // Assert
+            result.Should().NotBeNull();
+        }
+    }
+}
